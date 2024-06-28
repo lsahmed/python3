719ebbd04eba1436f593fe3e840362554992279a
Ahmed <ardyop7@gmail.com>
2024-06-28 15:00:25 +0000

    added basic questions to practice folders

---
+++ practice(Basic++)/sum.py
@@ -0,0 +1,8 @@
+# Write a program to sum a list with 4 numbers.
+a = int(input("Enter the number: "))  
+b = int(input("Enter the number: ")) 
+c = int(input("Enter the number: ")) 
+d = int(input("Enter the number: ")) 
+list = [a,b,c,d]
+sum = sum(list)
+print(sum)

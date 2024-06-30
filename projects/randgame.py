dbe18e9f54adf50846a386684ef2f7b9dadc8cde
Ahmed <ardyop7@gmail.com>
2024-06-30 14:55:13 +0000

    Added a lot of things such as a random number guessing game in projects/ folder

---
+++ projects/randgame.py
@@ -0,0 +1,8 @@
+import random
+
+num = random.randint(1,5)
+inp = int(input("Guess the number between 1 and 5: "))
+if(inp==num):
+  print("you guess correctly")
+else:
+  print("better luck next time")
